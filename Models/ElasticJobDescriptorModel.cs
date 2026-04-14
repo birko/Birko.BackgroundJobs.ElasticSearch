529aef4c2ac92817964c4e850de6b1f444f5fe40
František Bereň <birko@live.com>
2026-04-14 16:33:40 +0200

    fix: Add missing using directive for Birko.BackgroundJobs.Models in ElasticJobDescriptorModel

--- Models/ElasticJobDescriptorModel.cs
+++ Models/ElasticJobDescriptorModel.cs
@@ -1,6 +1,7 @@
 using System;
 using Birko.Data.Models;
 using Birko.BackgroundJobs.Serialization;
+using Birko.BackgroundJobs.Models;
 using Nest;
 
 namespace Birko.BackgroundJobs.ElasticSearch.Models;
